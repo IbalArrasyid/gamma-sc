dd689d34bfe85e6ae96405724a978cfd2b94d4ad
IbalArrasyid <107937107+IbalArrasyid@users.noreply.github.com>
2022-11-11 15:07:20 +0700

    Update form-action.php

--- form-action.php
+++ form-action.php
@@ -13,13 +13,14 @@ if ($conn->connect_error) {
     die("something went wrong try again: " . $conn->connect_error);
   }
 
-  $stmt = $conn->prepare("insert into form_message"(name, email, phone, message) value(?, ?, ?, ?)");
+  $stmt = $conn->prepare("insert into form_message(name, email, phone, message) value(?, ?, ?, ?)");
   $stmt->bind_param("ssss", $name, $email, $phone, $message);
 
   $name = "iqbal";
   $email = "iqbal@gmail.com";
   $phone = "81310946588";
   $message = "Hallo!. Im Interesting";
+  $stmt->execute();
 
 echo "New records created successfully";
 
