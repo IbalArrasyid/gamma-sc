52d58294314d28672c0dbdc3b576f8a15b4b011f
IbalArrasyid <107937107+IbalArrasyid@users.noreply.github.com>
2023-01-17 14:41:26 +0700

    Update form-action.php

--- form-action.php
+++ form-action.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "gamma-sc.com"; // or the hostname or IP address of the MySQL server
+$servername = "hostname or IP address of the MySQL server";
 $username = "username";
 $password = "password";
 $dbname = "form_db";
@@ -8,24 +8,33 @@ $dbname = "form_db";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
 // Check if the form was submitted
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $phone = $_POST['phone'];
   $message = $_POST['message'];
-  
-  // Insert the data into the database
-  $sql = "INSERT INTO form_data (name, email, phone, message) VALUES (?, ?, ?, ?)";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ssss", $name, $email, $phone, $message);
-  $stmt->execute();
+
+  // Insert form data into the database
+  $sql = "INSERT INTO form (name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Form data was successfully saved to the database.";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  // Send email
+  $to = "office@gamma-sc.com";
+  $subject = "New Form Submission";
+  $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
+  $headers = "From: $email";
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Your message has been sent!";
+  } else {
+    echo "There was an error sending your message. Please try again";
+  }
 }
 
 $conn->close();
+
 ?>
